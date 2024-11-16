01967e3e6da07e6636368434441595709377ff04
a7u7a <esteban@esrs.co>
2024-11-17 00:17:24 +0100

    Revived this after not using it for a while

--- optimize_images.py
+++ optimize_images.py
@@ -1,12 +1,6 @@
-import PIL
 from PIL import Image, ImageCms
 import os
 import io
-
-main_path = "/Users/userfriendly/Dropbox/Proyectos/esrs-2023/content/"
-out_path = "/Users/userfriendly/Dropbox/Proyectos/esrs-2023/content-optimized/"
-
-# save images to low-res folder
 
 
 def get_base_filename(path):
@@ -35,67 +29,72 @@ def get_icc_profile(image):
         return None
 
 
-def resize_images(in_path, out_path, thumb_path,folder_name, max_size, max_low_res_size):
+def resize_images(in_path, out_path, folder_name, max_size, max_low_res_size):
     images = os.listdir(in_path)
     counter = 0
     for filename in images:
         if filename == '.DS_Store':
             continue
-        print("processing:", filename)
+        print("Processing:", filename)
         image = Image.open(in_path + filename)
         icc = get_icc_profile(image)
 
-        # if width is larger than height
+        # Resize high-res version
         if image.size[0] > image.size[1]:
             ratio = find_ratio(max_size, image.size[0], image.size[1])
             small_size = int(image.size[1]*ratio)
             image = image.resize((max_size, small_size))
-            
-            low_res_ratio = find_ratio(max_low_res_size, image.size[0], image.size[1])
-            low_res_small_size = int(image.size[1]*low_res_ratio)
-            low_res_image = image.resize((low_res_small_size, max_low_res_size))
         else:
             ratio = find_ratio(max_size, image.size[1], image.size[0])
             small_size = int(image.size[0]*ratio)
             image = image.resize((small_size, max_size))
 
-            low_res_ratio = find_ratio(max_low_res_size, image.size[1], image.size[0])
-            low_res_small_size = int(image.size[0]*low_res_ratio)
-            low_res_image = image.resize((low_res_small_size, max_low_res_size))
+        # Create and resize low-res version
+        low_res_image = image.copy()
+        if low_res_image.size[0] > low_res_image.size[1]:
+            ratio = find_ratio(max_low_res_size, low_res_image.size[0], low_res_image.size[1])
+            small_size = int(low_res_image.size[1]*ratio)
+            low_res_image = low_res_image.resize((max_low_res_size, small_size))
+        else:
+            ratio = find_ratio(max_low_res_size, low_res_image.size[1], low_res_image.size[0])
+            small_size = int(low_res_image.size[0]*ratio)
+            low_res_image = low_res_image.resize((small_size, max_low_res_size))
 
-        final_out_path = thumb_path + 'thumb.webp'
-        low_res_path = out_path + '/low-res/' + \
-            folder_name + '_' + str(counter) + '.webp'
+        # Save both versions
+        high_res_path = out_path + folder_name + '_' + str(counter) + '.webp'
+        low_res_path = out_path + 'low-res/' + folder_name + '_' + str(counter) + '.webp'
 
-        # print("processing file:",filename,"resolution pre:",size_pre ,"resolution post:",image.size, "saving as:",final_out_path )
-        
-        image.save(final_out_path, 'webp', optimize=True,
-                   quality=75, icc_profile=icc)
-        #   Save low-res version
-
-        # low_res_image.save(low_res_path, 'webp', optimize=True,
-        #                    quality=90, icc_profile=icc)
+        image.save(high_res_path, 'webp', optimize=True, quality=75, icc_profile=icc)
+        low_res_image.save(low_res_path, 'webp', optimize=True, quality=90, icc_profile=icc)
         counter += 1
-
 
 def process_all(main_path, out_path):
     folders = os.listdir(main_path)
-    # create main output dir
-    os.mkdir(out_path)
+    os.makedirs(out_path, exist_ok=True)
+    
     for folder in folders:
-        print("Project:", folder)
         if folder == '.DS_Store':
             continue
-        # get image from "seleccion"
-        in_path = main_path + folder + "/thumb/"
-        sub_out_path = out_path + folder + '/'
-        sub_thumb_path = sub_out_path
-        low_res_path = sub_out_path + '/low-res/'
-        os.mkdir(sub_out_path)
-        # os.mkdir(sub_thumb_path)
-        # os.mkdir(low_res_path)
-        resize_images(in_path, sub_out_path,sub_thumb_path, folder, 2000, 50)
-
+            
+        in_path = os.path.join(main_path, folder, "selection/")
+        sub_out_path = os.path.join(out_path, folder)
+        low_res_path = os.path.join(sub_out_path, 'low-res')
+        
+        if not os.path.exists(in_path):
+            print(f"Error: Input directory '{in_path}' does not exist!")
+            continue
+        
+        os.makedirs(sub_out_path, exist_ok=True)
+        os.makedirs(low_res_path, exist_ok=True)
+        
+        resize_images(in_path, sub_out_path + '/', folder, 2000, 50)
 
 if __name__ == "__main__":
-    process_all(main_path, out_path)
+    import argparse
+    
+    parser = argparse.ArgumentParser(description='Optimize images for web use')
+    parser.add_argument('input_path', help='Path to input directory')
+    parser.add_argument('output_path', help='Path to output directory')
+    
+    args = parser.parse_args()
+    process_all(args.input_path, args.output_path)
